fe69c84a67c150d2587a58b724bc48241addb3d7
Vivek <test@test.com>
2024-03-17 23:49:06 +0530

    remove android framework test dependencies from no app modules

--- core-data/build.gradle.kts
+++ core-data/build.gradle.kts
@@ -26,6 +26,4 @@ dependencies {
     implementation("com.github.skydoves:sandwich-retrofit:2.0.5")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
